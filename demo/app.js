c64e7d3fcffc0ba1fafa560efd5b72929b5f967a
Zhuk, Alexander <Alexander.Zhuk@landsend.com>
2015-12-24 19:15:27 +0300

    initial data grid added2

---
+++ demo/app.js
@@ -0,0 +1,35 @@
+angular.module('myApp', ['dataGrid'])
+    .controller('myAppController', ['$scope', 'myAppFactory', function ($scope, myAppFactory) {
+
+        $scope.getData = function () {
+            myAppFactory.getData()
+                .success(function (response) {
+                    $scope.gridOptions.data = response;
+                    $scope.dataLoaded = true;
+                }).error(function () {
+            });
+        };
+        $scope.getData();
+
+        $scope.gridOptions = {
+            data: $scope.items,
+            urlSync: true
+        };
+
+        //$scope.items = [
+        //    {"firstName": "John", "lastName": "Doe"},
+        //    {"firstName": "Anna", "lastName": "Smith"},
+        //    {"firstName": "Peter", "lastName": "Jones"}
+        //];
+
+
+    }])
+    .factory('myAppFactory', function ($http) {
+        var root = 'http://jsonplaceholder.typicode.com';
+        return {
+            getData: function () {
+                return $http.get(root + '/posts', {});
+            }
+        }
+    });
+
