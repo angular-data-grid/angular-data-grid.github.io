3c42a0ebbb3eb3a28d74f780299a5971b6434ccf
Zhuk, Alexander <Alexander.Zhuk@landsend.com>
2016-01-27 14:25:55 +0300

    100k demo - changing autogenerated data to json file

--- demo/100k/js/demoApp.js
+++ demo/100k/js/demoApp.js
@@ -2,33 +2,39 @@
     'use strict';
 
     angular.module('myApp', ['ui.bootstrap', 'dataGrid', 'pagination'])
-        .controller('myAppController', ['$scope', function ($scope) {
+        .controller('myAppController', ['$scope', '$http', function ($scope, $http) {
 
             $scope.gridOptions = {
-                data: generateJSON(100000),
+                data: [],
                 urlSync: true
             };
 
+            $http({
+                method: 'GET',
+                url: 'http://angular-data-grid.github.io/demo/100k.json'
+            }).then(function successCallback(response) {
+                $scope.gridOptions.data = response.data;
+            });
         }]);
 
-    function generateJSON(length) {
-        var jsonObj = [],
-            i,
-            max,
-            names = ['Ann', 'Ben', 'Patrick', 'Steve', 'Fillip', 'Bob'],
-            item;
-        for (i = 0, max = length; i < max; i++) {
-
-            item = {};
-            item.id = i;
-            item.name = names[Math.round(Math.random() * (names.length - 1))];
-            item.phone = '+375-29-' + Math.round(Math.random() * 1000000);
-            item.date = Math.round(Math.random() * 1000000000000);
-            jsonObj.push(item);
-
-        }
-        return jsonObj;
-    }
+    //function generateJSON(length) {
+    //    var jsonObj = [],
+    //        i,
+    //        max,
+    //        names = ['Ann', 'Ben', 'Patrick', 'Steve', 'Fillip', 'Bob'],
+    //        item;
+    //    for (i = 0, max = length; i < max; i++) {
+    //
+    //        item = {};
+    //        item.id = i;
+    //        item.name = names[Math.round(Math.random() * (names.length - 1))];
+    //        item.phone = '+375-29-' + Math.round(Math.random() * 1000000);
+    //        item.date = Math.round(Math.random() * 1000000000000);
+    //        jsonObj.push(item);
+    //
+    //    }
+    //    return jsonObj;
+    //}
 })();
 
 
