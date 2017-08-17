fa133e63aa54fc8260d445f19f69978c40452498
Alexander Zhuk <AZhuk@lifetimefitness.com>
2017-08-17 15:30:43 +0300

    Export grid data #54 fix - feature added

--- demo/bootstrap/js/demoApp.js
+++ demo/bootstrap/js/demoApp.js
@@ -1,5 +1,5 @@
 angular.module('myApp', ['ui.bootstrap', 'dataGrid', 'pagination'])
-    .controller('myAppController', ['$scope', 'myAppFactory', function ($scope, myAppFactory) {
+    .controller('myAppController', ['$scope', 'myAppFactory', '$filter', function ($scope, myAppFactory, $filter) {
 
         $scope.gridOptions = {
             data: [],
@@ -9,6 +9,19 @@ angular.module('myApp', ['ui.bootstrap', 'dataGrid', 'pagination'])
         myAppFactory.getData().then(function (responseData) {
             $scope.gridOptions.data = responseData.data;
         });
+
+        $scope.exportToCsv = function (currentData) {
+            var exportData = [];
+            currentData.forEach(function (item) {
+                exportData.push({
+                    'Code': item.code,
+                    'Date Placed': $filter('date')(item.placed, 'shortDate'),
+                    'Status': item.statusDisplay,
+                    'Total': item.total.formattedValue
+                });
+            });
+            JSONToCSVConvertor(exportData, 'Export', true);
+        }
 
     }])
     .factory('myAppFactory', function ($http) {
